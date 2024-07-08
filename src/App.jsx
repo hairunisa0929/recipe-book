134d161ccf23922d24b9d79455e5995601d1fdb2
Hairunisa <143789547+hairunisa0929@users.noreply.github.com>
2024-07-08 10:17:27 +0700

    map cards and style the main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import Card from "./components/card/Card";
+import { recipes } from "./data/recipes";
+import "./app.css";
+
 function App() {
-  
-
   return (
-    <>
+    <div>
+      <h1>Recipe Book</h1>
 
-    </>
-  )
+      <div className="main">
+        {recipes.map((recipe) => (
+          <Card key={recipe.id} name={recipe.name} image={recipe.image} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
